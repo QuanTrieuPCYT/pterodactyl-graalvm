2789198166d8f201a80d257bb21cf0507c2d905d
QuanTrieuPCYT <admin@qtpc.tech>
2024-06-20 10:20:37 +0700

    23-OC, changes base OS for all images

--- eggtags.sh
+++ eggtags.sh
@@ -11,6 +11,7 @@ echo "22-JDK
 20-JDK
 22-OC
 22-temurin
+23-OC
 23-temurin" | sed -re 's/(.*)$/GraalVM &|ghcr.io\/quantrieupcyt\/pterodactyl-graalvm:&/'
 
 # Copy this list into dashboard.acme.co/admin/nests/egg/<the egg ofc>
@@ -27,6 +28,7 @@ GraalVM 17-EE|ghcr.io/quantrieupcyt/pterodactyl-graalvm:17-EE
 GraalVM 19-EE|ghcr.io/quantrieupcyt/pterodactyl-graalvm:19-EE
 GraalVM 20-JDK|ghcr.io/quantrieupcyt/pterodactyl-graalvm:20-JDK
 GraalVM 22-OC|ghcr.io/quantrieupcyt/pterodactyl-graalvm:22-OC
+GraalVM 23-OC|ghcr.io/quantrieupcyt/pterodactyl-graalvm:23-OC
 Temurin 22|ghcr.io/quantrieupcyt/pterodactyl-graalvm:22-temurin
 Temurin 21|ghcr.io/quantrieupcyt/pterodactyl-graalvm:21-temurin
 Temurin 23|ghcr.io/quantrieupcyt/pterodactyl-graalvm:23-temurin
